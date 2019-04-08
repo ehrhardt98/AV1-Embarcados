3a00981e7ab3d7632321a4ac5aee0fe4a64dee19
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2019-04-08 17:50:04 -0300

    C completo

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -16,6 +16,12 @@
 #define LED_IDX       8u
 #define LED_IDX_MASK  (1u << LED_IDX)
 
+// Botão2
+#define BUT2_PIO      PIOC
+#define BUT2_PIO_ID   ID_PIOC
+#define BUT2_IDX  31
+#define BUT2_IDX_MASK (1 << BUT2_IDX)
+
 // Botão3
 #define BUT3_PIO      PIOA
 #define BUT3_PIO_ID   ID_PIOA
@@ -25,11 +31,15 @@
 #define raio 0.325
 #define pi 3.14159265359
 
+volatile char buffer_time[32];
+
 struct ili9488_opt_t g_ili9488_display_opt;
 volatile Bool f_rtt_alarme = false;
+volatile Bool flag_rtc = false;
+volatile Bool flag_pause = false;
 volatile float counter_vel = 0.0;
 volatile float counter_dist = 0.0;
-volatile float vel = 133.0;
+volatile float vel = 0.0;
 volatile float dist = 0.0;
 volatile char buffer_vel[32];
 volatile char buffer_dist[32];
@@ -38,6 +48,8 @@ void pin_toggle(Pio *pio, uint32_t mask);
 void io_init(void);
 static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses);
 void configure_lcd(void);
+void RTC_init(void);
+void font_draw_text(tFont *font, const char *text, int x, int y, int spacing);
 
 void but3_callback(void)
 {
@@ -57,15 +69,41 @@ void RTT_Handler(void)
 
 	/* IRQ due to Alarm */
 	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+		ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH-1, ILI9488_LCD_HEIGHT-1);
 		pin_toggle(LED_PIO, LED_IDX_MASK);    // BLINK Led
 		vel = 3.6*raio*2*pi*counter_vel/4;
 		dist = 2*pi*raio*counter_dist;
-		sprintf(buffer_vel, "Velocidade: %f", vel);
-		sprintf(buffer_dist, "Distancia: %f", dist);
 		counter_vel = 0.0;
 		f_rtt_alarme = true;                  // flag RTT alarme
-		configure_lcd();
 	}
+}
+
+void RTC_Handler(void)
+{
+	uint32_t ul_status = rtc_get_status(RTC);
+
+	/*
+	*  Verifica por qual motivo entrou
+	*  na interrupcao, se foi por segundo
+	*  ou Alarm
+	*/
+	if ((ul_status & RTC_SR_SEC) == RTC_SR_SEC) {
+		rtc_clear_status(RTC, RTC_SCCR_SECCLR);
+		
+		flag_rtc = true;
+		
+	}
+	
+	/* Time or date alarm */
+	if ((ul_status & RTC_SR_ALARM) == RTC_SR_ALARM) {			
+		rtc_clear_status(RTC, RTC_SCCR_ALRCLR);
+	}
+	
+	rtc_clear_status(RTC, RTC_SCCR_ACKCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TIMCLR);
+	rtc_clear_status(RTC, RTC_SCCR_CALCLR);
+	rtc_clear_status(RTC, RTC_SCCR_TDERRCLR);
+	
 }
 
 void pin_toggle(Pio *pio, uint32_t mask){
@@ -83,17 +121,18 @@ void io_init(void){
 	pmc_enable_periph_clk(BUT3_PIO_ID);
 	pio_configure(BUT3_PIO, PIO_INPUT, BUT3_IDX_MASK, PIO_PULLUP | PIO_DEBOUNCE);
 	
-	pio_handler_set(BUT3_PIO,
-	BUT3_PIO_ID,
-	BUT3_IDX_MASK,
-	PIO_IT_RISE_EDGE,
-	but3_callback);
-	
 	pio_enable_interrupt(BUT3_PIO, BUT3_IDX_MASK);
 	pio_set_debounce_filter(BUT3_PIO, BUT3_IDX_MASK, 20);
 	
 	NVIC_EnableIRQ(BUT3_PIO_ID);
 	NVIC_SetPriority(BUT3_PIO_ID, 4); // Prioridade 4
+	
+	pio_handler_set(BUT3_PIO,
+	BUT3_PIO_ID,
+	BUT3_IDX_MASK,
+	PIO_IT_RISE_EDGE,
+	but3_callback);
+
 }
 
 static float get_time_rtt(){
@@ -121,6 +160,27 @@ static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
 	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN);
 }
 
+void RTC_init(){
+	/* Configura o PMC */
+	pmc_enable_periph_clk(ID_RTC);
+
+	/* Default RTC configuration, 24-hour mode */
+	rtc_set_hour_mode(RTC, 0);
+
+	/* Configura data e hora manualmente */
+	rtc_set_time(RTC, 0, 0, 0);
+
+	/* Configure RTC interrupts */
+	NVIC_DisableIRQ(RTC_IRQn);
+	NVIC_ClearPendingIRQ(RTC_IRQn);
+	NVIC_SetPriority(RTC_IRQn, 0);
+	NVIC_EnableIRQ(RTC_IRQn);
+
+	/* Ativa interrupcao via alarme */
+	rtc_enable_interrupt(RTC,  RTC_IER_SECEN);
+
+}
+
 void configure_lcd(void){
 	/* Initialize display parameter */
 	g_ili9488_display_opt.ul_width = ILI9488_LCD_WIDTH;
@@ -133,7 +193,6 @@ void configure_lcd(void){
 	ili9488_draw_filled_rectangle(0, 0, ILI9488_LCD_WIDTH-1, ILI9488_LCD_HEIGHT-1);
 	
 }
-
 
 void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
 	char *p = text;
@@ -156,17 +215,37 @@ int main(void) {
 	sysclk_init();	
 	io_init();
 	configure_lcd();
+	RTC_init();
+	//rtc_set_time_alarm(RTC, 1, a, 1, b, 1, c+1);
 	
 	f_rtt_alarme = true;
+	flag_rtc = true;
 	
 	while(1) {
-		if(f_rtt_alarme) {
-			uint16_t pllPreScale = (int) (((float) 32768) / 2.0);
-			uint32_t irqRTTvalue  = 8;
-			RTT_init(pllPreScale, irqRTTvalue);
-			font_draw_text(&calibri_36, buffer_vel, 10, 100, 1);
-			font_draw_text(&calibri_36, buffer_dist, 10, 200, 1);
-			f_rtt_alarme = false;
+		if(!flag_pause) {
+			
+			if(f_rtt_alarme) {
+				uint16_t pllPreScale = (int) (((float) 32768) / 2.0);
+				uint32_t irqRTTvalue  = 8;
+				RTT_init(pllPreScale, irqRTTvalue);
+				sprintf(buffer_vel, "Velocidade: %f", vel);
+				sprintf(buffer_dist, "Distancia: %f", dist);
+				font_draw_text(&calibri_36, buffer_vel, 10, 100, 1);
+				font_draw_text(&calibri_36, buffer_dist, 10, 200, 1);
+				f_rtt_alarme = false;
+			}
+			if(flag_rtc) {
+			
+				uint32_t a = 0;
+				uint32_t b = 0;
+				uint32_t c = 0;
+				rtc_get_time(RTC, &a, &b, &c);
+
+				sprintf(buffer_time, "Tempo: %d:%d:%d", a, b, c);
+				font_draw_text(&calibri_36, buffer_time, 10, 300, 1);
+			}
+			pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
 		}
+	pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
 	}
 }
